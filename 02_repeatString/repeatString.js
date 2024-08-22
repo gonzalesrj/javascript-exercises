a782d9feb0cf0b547909a13a28808c1fd9c60d67
Russel John Gonzales <russeljohn.gonzales@yahoo.com>
2024-08-22 13:24:38 -0400

    Add a function that takes a string and a number and generate the string times the number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(getWord, getNumber) {
+  let word = "";
+  if(getNumber < 0) return "ERROR";
+  
+  for(i = 0; i < getNumber; i++) {
+    word += getWord;
+  }
 
+  return word;
 };
 
 // Do not edit below this line
