968ea3135a07928c2c61b4b20479c9ce0dfeb7bd
Russel John Gonzales <russeljohn.gonzales@yahoo.com>
2024-08-23 02:03:11 -0400

    Add a function that reverses a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let splitWord = word.split("");
+  let reverseWordArray = splitWord.reverse();
+  let joinWordArray = reverseWordArray.join("");
 
+  return joinWordArray;
 };
 
 // Do not edit below this line
