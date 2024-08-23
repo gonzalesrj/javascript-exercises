a1188daa46edaea9feccc1652e4b9acb821a64f4
Russel John Gonzales <russeljohn.gonzales@yahoo.com>
2024-08-23 03:00:25 -0400

    Add a function that remove elements in an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeValue) {
+
+  const newArray = [];
+
+  array.forEach((item) => {
+    if(!removeValue.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+
+  return newArray;
 };
 
 // Do not edit below this line
