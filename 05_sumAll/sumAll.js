51ad049d121c117ff7f20df4e516d1df0e8f3715
Russel John Gonzales <russeljohn.gonzales@yahoo.com>
2024-08-23 03:23:13 -0400

    Add function that takes two numbers and return the sum of numbers in between

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(initialNumber, finalNumber) {
+
+  if(initialNumber < 0
+    || finalNumber < 0
+    || !Number.isInteger(initialNumber) 
+    || !Number.isInteger(finalNumber)) {
+    return "ERROR";
+  }
+
+  if(initialNumber > finalNumber) {
+    let temp = initialNumber;
+    initialNumber = finalNumber;
+    finalNumber = temp;
+  }
+
+  let sum = 0;
+
+  for(i = initialNumber; i <= finalNumber; i++) {
+    sum += initialNumber;
+    initialNumber++;
+  }
+
+  return sum;
 
 };
 
